636f605c94eba235f11818db0267d6ec3bf9dc0f
Nikolay Rozanov <ozcenick@gmail.com>
2022-03-01 12:55:40 -0800

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module ca.bcit.comp2522.termproject.idk {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens ca.bcit.comp2522.termproject.idk to javafx.fxml;
+    exports ca.bcit.comp2522.termproject.idk;
+}
