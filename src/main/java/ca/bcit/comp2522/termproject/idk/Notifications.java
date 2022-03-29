d5a1fe4e025dd988c763b6ea0221aefe71fa44ff
Prince H Chabveka <pchabveka@my.bcit.ca>
2022-03-28 22:25:28 -0700

    Remove redundant code

--- src/main/java/ca/bcit/comp2522/termproject/idk/Notifications.java
+++ src/main/java/ca/bcit/comp2522/termproject/idk/Notifications.java
@@ -1,6 +1,5 @@
 package ca.bcit.comp2522.termproject.idk;
 
-
 import javafx.scene.input.KeyCode;
 
 import static com.almasb.fxgl.dsl.FXGL.*;
@@ -16,5 +15,4 @@ import static com.almasb.fxgl.dsl.FXGL.*;
         protected void notification() {
             onKeyDown(KeyCode.N, "Notify", () -> getNotificationService().pushNotification("Hello Prince"));
         }
-
 }
