4724446b608b25d05e9cb91a92015d17c79a219e
OceNick <ozcenick@gmail.com>
2022-04-01 15:54:02 -0700

    Implement the EnemyInfo class

---
+++ src/main/java/ca/bcit/comp2522/termproject/idk/component/enemies/EnemyInfo.java
@@ -0,0 +1,23 @@
+package ca.bcit.comp2522.termproject.idk.component.enemies;
+
+/**
+ * Represents a utility class that stores enemy information as constants.
+ *
+ * @author Nikolay Rozanov.
+ * @version 2022
+ */
+public final class EnemyInfo {
+    /**
+     * All enemies constants.
+     */
+    static int ALL_ENEMIES_ATTACK_SPEED = 1;
+
+    /**
+     * Constants for both FireWizards and DarkWizards
+     */
+    static int WIZARD_DAMAGE = 15;
+    static int WIZARD_MOVE_SPEED = 50;
+
+
+    private EnemyInfo() { }
+}
