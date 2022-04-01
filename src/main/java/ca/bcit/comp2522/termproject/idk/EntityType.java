990617e272717967815d8ccac219a7145780d27f
OceNick <ozcenick@gmail.com>
2022-04-01 15:55:20 -0700

    Implement the EntityType enum

---
+++ src/main/java/ca/bcit/comp2522/termproject/idk/EntityType.java
@@ -0,0 +1,18 @@
+package ca.bcit.comp2522.termproject.idk;
+
+/**
+ * Represent different Entity types.
+ *
+ * @author Nikolay Rozanov
+ * @version 2022
+ */
+public enum EntityType {
+    /**
+     * Represents the player.
+     */
+    PLAYER,
+    /**
+     * Represents the enemy.
+     */
+    ENEMY
+}
