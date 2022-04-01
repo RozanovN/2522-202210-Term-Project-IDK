185555d338960603298e09e2a83b1153468e3acd
OceNick <ozcenick@gmail.com>
2022-04-01 10:04:02 -0700

    Add FireWizard entity

--- src/main/java/ca/bcit/comp2522/termproject/idk/GameEntitiesFactory.java
+++ src/main/java/ca/bcit/comp2522/termproject/idk/GameEntitiesFactory.java
@@ -1,6 +1,8 @@
 package ca.bcit.comp2522.termproject.idk;
 
+import ca.bcit.comp2522.termproject.idk.component.enemies.WizardComponent;
 import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.dsl.components.HealthIntComponent;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.EntityFactory;
 import com.almasb.fxgl.entity.SpawnData;
@@ -10,6 +12,8 @@ import com.almasb.fxgl.physics.BoundingShape;
 import com.almasb.fxgl.physics.HitBox;
 import com.almasb.fxgl.physics.PhysicsComponent;
 import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
+import com.almasb.fxgl.physics.box2d.dynamics.FixtureDef;
+import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -104,20 +108,20 @@ public class GameEntitiesFactory implements EntityFactory{
                 .with(new PhysicsComponent(), new CollidableComponent())
                 .build();
     }
-//
-//    /**
-//     * Demo of player for testing purpose, to be changed later.
-//     *
-//     * @return Entity representing player
-//     */
-//    @Spawns("Player")
-//    public Entity newPlayer() {
-//        PhysicsComponent physicsComponent = new PhysicsComponent();
-//        physicsComponent.setBodyType(BodyType.DYNAMIC);
-//        return FXGL
-//                .entityBuilder()
-//                .view(new Rectangle(25, 25, Color.BLUE))
-//                .with(physicsComponent, new CollidableComponent())
-//                .build();
-//    }
+
+    @Spawns("FireWizard")
+    public Entity newFireWizard(SpawnData data) {
+        PhysicsComponent physicsComponent = new PhysicsComponent();
+        physicsComponent.setBodyType(BodyType.DYNAMIC);
+        physicsComponent.addGroundSensor(new HitBox("GROUND_SENSOR", new Point2D(4, 64),
+                BoundingShape.box(6, 12)));
+        physicsComponent.setFixtureDef(new FixtureDef().friction(100f));
+
+        return FXGL
+                .entityBuilder()
+                .bbox(new HitBox(new Point2D(55,50), BoundingShape.box(35, 50)))
+                .at(25, 1)
+                .with(physicsComponent, new CollidableComponent(true), new WizardComponent(), new HealthIntComponent())
+                .build();
+    }
 }
