8bcb12445e2ec1179d497e50b353a28345529a21
OceNick <ozcenick@gmail.com>
2022-04-03 12:54:05 -0700

    Create the AttackComponent class

---
+++ src/main/java/ca/bcit/comp2522/termproject/idk/component/AttackComponent.java
@@ -0,0 +1,58 @@
+package ca.bcit.comp2522.termproject.idk.component;
+
+import com.almasb.fxgl.entity.component.Component;
+
+/**
+ * Represent Attack Component.
+ *
+ * @author Nikolay Rozanov
+ * @version 2022
+ */
+public class AttackComponent extends Component {
+    private int damage;
+
+    /**
+     * Constructs an AttackComponent.
+     *
+     * @param damage an int representing damage value
+     */
+    public AttackComponent(final int damage) {
+        this.damage = damage;
+    }
+
+    /**
+     * Gets this AttackComponent's damage.
+     *
+     * @return an int representing this AttackComponent's damage
+     */
+    public int getDamage() {
+        return damage;
+    }
+
+    /**
+     * Sets this AttackComponent's damage.
+     *
+     * @param damage an int representing this AttackComponent's damage.
+     */
+    public void setDamage(final int damage) {
+        this.damage = damage;
+    }
+
+    /**
+     * Multiples this AttackComponent's damage by the given percentage
+     *
+     * @param percentage a double representing the percentage multiplier
+     */
+    public void changeDamageByPercentage(final double percentage) {
+        this.damage = (int) (damage * percentage);
+    }
+
+    /**
+     * Changes this AttackComponent's damage by the given value
+     *
+     * @param value an int representing the value
+     */
+    public void changeDamageByValue(final double value) {
+        this.damage += value;
+    }
+}
