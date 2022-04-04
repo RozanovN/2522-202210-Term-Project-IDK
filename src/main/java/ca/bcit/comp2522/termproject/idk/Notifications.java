e9db15be16f5e0a821bee794b405276508f87284
Prince H Chabveka <pchabveka@my.bcit.ca>
2022-04-03 21:58:08 -0700

    Remove sound extension

--- src/main/java/ca/bcit/comp2522/termproject/idk/Notifications.java
+++ src/main/java/ca/bcit/comp2522/termproject/idk/Notifications.java
@@ -8,15 +8,19 @@ import static com.almasb.fxgl.dsl.FXGL.*;
  * Add various notifications to the user.
  *
  * @author Prince Chabveka
- * @author Nikolay Rozanov
  * @version 2022
  */
     public class Notifications {
 
-        /**
+    public Notifications() {
+    }
+
+    /**
          * Greet user with notification
          */
         protected void notification() {
-            onKeyDown(KeyCode.N, "Notify", () -> getNotificationService().pushNotification("Hello Prince"));
+            onKeyDown(KeyCode.N, "Notify", () -> getNotificationService().pushNotification("Hello Prince")
+            );
         }
+
 }
