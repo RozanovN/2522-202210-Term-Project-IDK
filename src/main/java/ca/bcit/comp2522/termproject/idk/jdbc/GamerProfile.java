7970397a9521de67c1db248d7981e3bee6c51921
Prince H Chabveka <pchabveka@my.bcit.ca>
2022-04-09 21:48:44 -0700

    Add documentation

--- src/main/java/ca/bcit/comp2522/termproject/idk/jdbc/GamerProfile.java
+++ src/main/java/ca/bcit/comp2522/termproject/idk/jdbc/GamerProfile.java
@@ -1,5 +1,15 @@
 package ca.bcit.comp2522.termproject.idk.jdbc;
 
+
+import com.almasb.fxgl.entity.component.Component;
+
+import java.util.Objects;
+
+/**
+ * Represents gamer's profile.
+ * @author Prince Chabveka
+ * @version 2022
+ */
     public class GamerProfile {
 
         private int id;
@@ -20,5 +30,27 @@ package ca.bcit.comp2522.termproject.idk.jdbc;
         public void setName(String name) {
             this.name = name;
         }
+
+
+    /**
+     * Gamers are the same if they have the same id, username and name.
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GamerProfile that = (GamerProfile) o;
+        return id == that.id && Objects.equals(name, that.name);
     }
+
+    /**
+     * Generate hashcode
+     * @return an int
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
 
