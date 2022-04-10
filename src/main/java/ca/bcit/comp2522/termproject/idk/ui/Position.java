242f8bc020badfe74d0ca64dbeb9a5b2daa7b26c
Prince H Chabveka <pchabveka@my.bcit.ca>
2022-04-09 22:18:50 -0700

    Add documentation

--- src/main/java/ca/bcit/comp2522/termproject/idk/ui/Position.java
+++ src/main/java/ca/bcit/comp2522/termproject/idk/ui/Position.java
@@ -1,8 +1,16 @@
 package ca.bcit.comp2522.termproject.idk.ui;
 
+import com.almasb.fxgl.entity.component.Component;
+
 /**
- *Represent
+ * Represents a Component of the player's Entity.
+ * @author Prince Chabveka
+ * @author Nikolay Rozanov
+ * @version 2022
  */
 public enum Position {
+    /**
+     *Represents player position
+     */
     LEFT, RIGHT, TOP, BOTTOM
 }
